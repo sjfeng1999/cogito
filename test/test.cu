d7f99b7410ac6e741631d4aef8220b2cf206fac1
Feng Shijie <j514681085@icloud.com>
2022-06-29 00:54:24 +0800

    :pencil2: [test] test all kernel

--- test/test.cu
+++ test/test.cu
@@ -7,9 +7,9 @@
 
 #define COGITO_ENBALE_KERNEL_PROFILER 
 
-// #include "unittest/general/test_general.cuh"
+#include "unittest/general/test_general.cuh"
 #include "unittest/blas/test_blas.cuh"
-// #include "unittest/dnn/test_dnn.cuh"
+#include "unittest/dnn/test_dnn.cuh"
 
 int main(){
     testing::InitGoogleTest();
